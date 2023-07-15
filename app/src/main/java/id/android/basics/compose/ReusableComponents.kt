8b6ad76580833256840d3bf4cdf9a4734f17176d
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-07-15 17:19:32 +0700

    Codelabs - Compose for Wear OS: Add Wear Unique Composables

--- app/src/main/java/id/android/basics/compose/ReusableComponents.kt
+++ app/src/main/java/id/android/basics/compose/ReusableComponents.kt
@@ -10,19 +10,30 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Message
 import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.SelfImprovement
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.AppCard
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Switch
 import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.ToggleChip
 import id.android.basics.compose.theme.ComposerTheme
 
 /**
@@ -93,12 +104,49 @@ fun CardExample(
 fun ChipExample(
   modifier: Modifier = Modifier,
   iconModifier: Modifier = Modifier) {
-  // TODO: Create a Chip Composable
+  Chip(
+    modifier = modifier,
+    onClick = { /* no content */ },
+    label = {
+      Text(
+        text = "5 minute Meditation",
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+      )
+    },
+    icon = {
+      Icon(
+        imageVector = Icons.Rounded.SelfImprovement,
+        contentDescription = "Triggers meditation action",
+        modifier = iconModifier
+      )
+    }
+  )
 }
 
 @Composable
 fun ToggleChipExample(modifier: Modifier = Modifier) {
-  // TODO: Create a ToggleChip Composable
+  var checked by remember { mutableStateOf(true) }
+  ToggleChip(
+    modifier = modifier,
+    checked = checked,
+    toggleControl = {
+      Switch(
+        checked = checked,
+        modifier = Modifier.semantics {
+          this.contentDescription = if (checked) "On" else "Off"
+        }
+      )
+    },
+    onCheckedChange = { checked = it },
+    label = {
+      Text(
+        text = "Sound",
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+      )
+    }
+  )
 }
 
 /**
